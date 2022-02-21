fa0b3d42f129ce6e6b9728507ed66a44d9a51b0d
Jacob <bjacobcode@gmail.com>
2022-02-20 16:36:48 -0800

    intial commit

---
+++ BestBuy_CRUD/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BestBuy_CRUD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
