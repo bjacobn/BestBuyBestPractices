74fee27b891d98cf9833c4d308cdf25643e051b0
Jacob <bjacobcode@gmail.com>
2022-02-22 10:46:15 -0800

    Setup Product Dapper connection

---
+++ BestBuy_CRUD/DapperProductRepository.cs
@@ -0,0 +1,51 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace BestBuy_CRUD
+{
+    public class DapperProductRepository : IProductRepository
+    {
+        private readonly IDbConnection _connection;
+
+        // Custom Constructor
+        public DapperProductRepository(IDbConnection connection)
+        {
+            _connection = connection;
+        }
+
+        //Read Data
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return _connection.Query<Product>("SELECT * FROM PRODUCTS;");
+        }
+
+        //Create Data
+        public void CreateProduct(string name, double price, int categoryID)
+        {
+
+            _connection.Execute("INSERT INTO products (Name, Price, CategoryID) VALUES (@name, @price, @categoryID); ", new { name = name, price = price, categoryID = categoryID });
+        }
+
+        //Update Data
+        public void UpdateProduct(int productID, string updatedName)
+        {
+            _connection.Execute("UPDATE products SET name = @updatedName WHERE ProductID = @productID;", new { updatedName = updatedName, productID = productID });
+        }
+
+        //Delete Data
+        public void DeleteProduct(int productID)
+        {
+            //Delete sequence requires primary ke
+            _connection.Execute("DELETE FROM sales WHERE ProductID = @productID;", new { productID = productID });
+
+            _connection.Execute("DELETE FROM reviews WHERE ProductID = @productID;", new { productID = productID });
+
+            _connection.Execute("DELETE FROM products WHERE ProductID = @productID;", new { productID = productID });
+
+        }
+    }
+}
+}
--- BestBuy_CRUD/IProductRepository.cs
+++ BestBuy_CRUD/IProductRepository.cs
@@ -4,7 +4,16 @@ using System.Text;
 
 namespace BestBuy_CRUD
 {
-    interface IProductRepository
+   public interface IProductRepository
     {
+        //Abstract methods
+        IEnumerable<Product> GetAllProducts();
+
+        void CreateProduct(string name, double price, int categoryID);
+
+        void UpdateProduct(int productID, string updatedName);
+
+        void DeleteProduct(int productID);
+
     }
 }
